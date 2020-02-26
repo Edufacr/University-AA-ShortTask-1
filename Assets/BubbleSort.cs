1d69a52567c70e3cb22a5712c1318bf1ddad43af
Eduardo <edumadrim@gmail.com>
2020-02-26 11:30:09 -0600

    BubbleSort completed

--- Assets/BubbleSort.cs
+++ Assets/BubbleSort.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using System;
 using System.IO;
 using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+using Random = System.Random;
 
 // C# program for implementation 
 // of Bubble Sort
@@ -13,48 +15,57 @@ public class BubbleSort : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-	    int []arr = {64, 34, 25, 12, 22, 11, 90}; 
-	    Stopwatch s = Stopwatch.StartNew();
-	    bubbleSort(arr); 
-	    Console.WriteLine("Time taken: " + s.ElapsedMilliseconds + "ms");
-	    printArray(arr); 
+        Debug.Log("hola");
+        int currentN = 0;
+        Stopwatch s;
+        Random rand = new Random();
+        for (int i = 0; i < 10; i++)
+        {
+            currentN += 10000;
+            for (int j = 0; j < 3; j++)
+            {
+                int[] array = getRanArray(currentN,rand);
+                s = Stopwatch.StartNew();
+                bubbleSort(array);
+                Debug.Log("X = "+currentN+" run: "+j+" time: "+s.ElapsedMilliseconds);
+                s.Stop();
+            }
+        }
     }
 	
-	void runTimes(int startN, int endN, int increaseVal){
-		Stopwatch s;
-		for(int n = startN; n < endN; n + increaseVal){
-			s = Stopwatch.StartNew();
-			//Create array based on N
-			int []array;
-			bubbleSort(array):
-
-
-		}
-	}
+    int[] getRanArray(int pCurrentN, System.Random pRand){
+		
+        int[] randArr = new int[pCurrentN];
+        int n = randArr.Length;
+        for (int i=0; i<n; i++){
+            randArr[i] = pRand.Next(1,101);
+        } 
+        return randArr;
+    }
 
     static void bubbleSort(int []arr) 
-    	{ 
-    		int n = arr.Length; 
-    		for (int i = 0; i < n - 1; i++) 
-    			for (int j = 0; j < n - i - 1; j++) 
-    				if (arr[j] > arr[j + 1]) 
-    				{ 
-    					// swap temp and arr[i] 
-    					int temp = arr[j]; 
-    					arr[j] = arr[j + 1]; 
-    					arr[j + 1] = temp; 
-    				} 
-    	} 
+    { 
+        int n = arr.Length; 
+        for (int i = 0; i < n - 1; i++) 
+        for (int j = 0; j < n - i - 1; j++) 
+            if (arr[j] > arr[j + 1]) 
+            { 
+                // swap temp and arr[i] 
+                int temp = arr[j]; 
+                arr[j] = arr[j + 1]; 
+                arr[j + 1] = temp; 
+            } 
+    } 
     
-    	/* Prints the array */
-    	static void printArray(int []arr) 
-    	{ 
-    		int n = arr.Length; 
-    		for (int i = 0; i < n; ++i) 
-    			Console.Write(arr[i] + " "); 
-    		Console.WriteLine(); 
-    	}
+    /* Prints the array */
+    static void printArray(int []arr) 
+    { 
+        int n = arr.Length; 
+        for (int i = 0; i < n; ++i) 
+            Console.Write(arr[i] + " "); 
+        Console.WriteLine(); 
+    }
 
 } 
     
-    // This code is contributed by Sam007 
+// This code is contributed by Sam007 
